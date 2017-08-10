54d7d7b2533f074f4bf95519891ece84a462df16
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-08-10 11:58:42 +0700

    mengembalikan nilai

---
+++ SampleCSharp1/HelperMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleCSharp1
+{
+    public class HelperMethod
+    {
+        public double HitungLuasSegitiga(double alas,double tinggi)
+        {
+            double hasil = 0.5 * alas * tinggi;
+            return hasil;
+        }
+
+        public void HitungLuasLingkaran(double jari)
+        {
+            double hasil = Math.PI * jari * jari;
+            Console.WriteLine("Luas : " + hasil);
+        }
+    }
+}
--- SampleCSharp1/Program.cs
+++ SampleCSharp1/Program.cs
@@ -7,7 +7,7 @@ namespace SampleCSharp1
     {
         static void Main(string[] args)
         {
-            int bil1 = 12;
+            /*int bil1 = 12;
             int bil2 = bil1;
 
             bil2 = bil2 + 5;
@@ -49,8 +49,92 @@ namespace SampleCSharp1
             for(int i=0;i<3;i++)
             {
                 Console.WriteLine(arrMatkul[i]);
+            }*/
+
+            /*int bil1 = 12;
+            int bil2 = 12;
+            bool cek = bil1 == bil2;
+            Console.WriteLine(cek);
+
+            Mahasiswa mhs1 = new Mahasiswa
+            {
+                Nim = "22998877",
+                Nama = "Budi",
+                IPK = 3.4
+            };
+
+            Mahasiswa mhs2 = new Mahasiswa
+            {
+                Nim = "22998877",
+                Nama = "Budi",
+                IPK = 3.4
+            };
+
+            object mhs3 = new object();
+
+            bool cekobjek = mhs1 is Mahasiswa;
+            Mahasiswa konversi = mhs3 as Mahasiswa;
+            Console.WriteLine(cekobjek);*/
+
+            int nilai = 0;
+
+            Console.Write("Masukan Nilai :");
+
+            try
+            {
+                nilai = Convert.ToInt32(Console.ReadLine());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
 
+            if (nilai >= 80)
+            {
+                Console.WriteLine("A");
+            }
+            else if (nilai >= 70)
+            {
+                Console.WriteLine("B");
+            }
+            else if (nilai >= 60)
+            {
+                Console.WriteLine("C");
+            }
+            else
+            {
+                Console.WriteLine("D");
+            }
+
+            string jurusan = "01";
+            switch (jurusan)
+            {
+                case "01":
+                    Console.WriteLine("TI");
+                    break;
+                case "02":
+                    Console.WriteLine("SI");
+                    break;
+                default:
+                    Console.WriteLine("Biology");
+                    break;
+            }
+
+            int bil = 0;
+            while (bil < 10)
+            {
+                bil++;
+                if (bil == 5)
+                    continue;
+
+                Console.WriteLine(bil);
+            }
+
+            /*do
+            {
+                bil++;
+                Console.WriteLine(bil);
+            } while (bil < 10);*/
         }
     }
 }
